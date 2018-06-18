a703414651d0eb425ebfb1929db56dc2ec2465f6
Jeff Wong <jeffmjwong@gmail.com>
2018-06-18 16:42:20 +1000

    Create main react component and pass props

---
+++ src/components/SkiDayCount.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../stylesheets/ui.scss';
+
+const percentToDecimal = decimal => `${decimal * 100}%`;
+
+const calcGoalProgress = (total, goal) => percentToDecimal(total / goal);
+
+const SkiDayCount = ({ total, powder, backcountry, goal }) => (
+  <div className='ski-day-count'>
+
+    <div className='total-days'>
+      <span>{ total }</span>
+      <span>days</span>
+    </div>
+
+    <div className='powder-days'>
+      <span>{ powder }</span>
+      <span>days</span>
+    </div>
+
+    <div className='backcountry-days'>
+      <span>{ backcountry }</span>
+      <span>days</span>
+    </div>
+
+    <div>
+      <span>{ calcGoalProgress(total, goal) }</span>
+    </div>
+
+  </div>
+);
+
+export default SkiDayCount;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { hello, goodbye } from './lib';
+import SkiDayCount from './components/SkiDayCount';
 
 render(
-  <div>
-    { hello }
-    { goodbye }
-  </div>,
+  <SkiDayCount
+    total={ 50 }
+    powder={ 20 }
+    backcountry={ 10 }
+    goal={ 100 } />,
   document.getElementById('react-container')
 );
