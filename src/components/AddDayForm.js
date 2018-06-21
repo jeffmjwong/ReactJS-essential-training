7f9c14bde8c723bc42b5a941f59c223ee9e9035f
Jeff Wong <jeffmjwong@gmail.com>
2018-06-21 18:20:57 +1000

    Populate add day form

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const AddDayForm = () => {
-  return (
-    <h1>Add A Day</h1>
-  );
+class AddDayForm extends Component {
+  render() {
+    const { resort, date, powder, backcountry } = this.props;
+    return (
+      <form className="add-day-form">
+        <label htmlFor="resort">Resort Name</label>
+        <input id="resort" type="text" defaultValue={ resort } required />
+
+        <label htmlFor="date">Date</label>
+        <input id="date" type="date" defaultValue={ date } required />
+
+        <div>
+          <input id="powder" type="checkbox" defaultChecked={ powder } />
+          <label htmlFor="powder">Powder Day</label>
+        </div>
+
+        <div>
+          <input id="backcountry" type="checkbox" defaultChecked={ backcountry } />
+          <label htmlFor="backcountry">Backcountry Day</label>
+        </div>
+      </form>
+    );
+  }
+}
+
+AddDayForm.defaultProps = {
+  resort: 'Kirkwood',
+  date: '2017-02-12',
+  powder: true,
+  backcountry: false
+};
+
+AddDayForm.propTypes = {
+  resort: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  powder: PropTypes.bool.isRequired,
+  backcountry: PropTypes.bool.isRequired
 };
 
 export default AddDayForm;
